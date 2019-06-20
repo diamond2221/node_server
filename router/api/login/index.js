4769570363ef3e297099c55e9dedfc28e0e9a6fc
张毓 <zhangyu@wyc.cn>
2019-06-20 14:44:23 +0800

    6/20 小修改

--- router/api/login/index.js
+++ router/api/login/index.js
@@ -3,7 +3,8 @@ const qs = require("querystring");
 
 module.exports = async (req, res) => {
   // console.log(req.body);
-  const { code } = req.body;
+  // const { code } = req.body;
+  const code = "08115KKN0L1eUa2yfRMN0QZCKN015KKk";
   const appid = "wxe812e5fa4ea4f9bf";
   const secret = "";
   const result = await axios.get(
